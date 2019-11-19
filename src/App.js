1d006b9db748a8951a72a2d7ab68d079280c50e1
Yury Lebedev <y.d.lebedev@gmail.com>
2019-11-19 15:53:51 -0500

    Add responsive layout

--- src/App.js
+++ src/App.js
@@ -5,27 +5,46 @@ import './css/App.css';
 import './css/Items.css';
 
 class App extends Component {
-  state = {
-    containers: [
-      { name: 'Developer', index: 0, data: DEV },
-      { name: 'Business', index: 1, data: BIZ },
-    ],
-    open: 0,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      containers: [
+        { name: 'Developer', index: 0, data: DEV },
+        { name: 'Business', index: 1, data: BIZ },
+      ],
+      open: 0,
+      windowWidth: 0,
+    };
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
 
-  swithExp() {
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions() {
+    this.setState({ windowWidth: typeof window !== 'undefined' ? window.innerWidth : 0 });
+  }
+
+  switchExp() {
     const { open } = this.state;
     this.setState({ open: open ? 0 : 1 });
   }
 
   render() {
     const sections = this.state.containers;
-    const { open } = this.state;
+    const { open, windowWidth } = this.state;
+    console.log(windowWidth);
     return (
       <div className="App">
-        <Header switch={() => this.swithExp()} />
+        <Header switch={() => this.switchExp()} />
         <div className="SectionWrapper">
-          <ExperienceContainer details={sections[open]} />
+          <ExperienceContainer details={sections[open]} width={windowWidth} />
         </div>
       </div>
     );
@@ -34,31 +53,33 @@ class App extends Component {
 
 class ExperienceContainer extends Component {
   render() {
-    let { details } = this.props;
+    let { details, width } = this.props;
     return (
       <div className="ExperienceContainer">
         {details.data.map(el => (
-          <Item {...el} />
+          <Item {...el} width={width} />
         ))}
       </div>
     );
   }
 }
 
+const phoneItem = {
+  flex: 1,
+  minHeight: '300px',
+  borderBottom: '1px solid #000000',
+};
+
 class Item extends Component {
   state = { mouseOver: false };
 
-  componentWillUnmount() {
-    console.log('unmounting');
-  }
-
   render() {
     const { props } = this;
-    console.log(props.class);
     const { mouseOver } = this.state;
+    console.log(mouseOver);
     return (
       <div
-        style={props.class}
+        style={props.width > 600 ? props.class : phoneItem}
         onMouseEnter={() => {
           console.log('enter', props.expName);
           this.setState({ mouseOver: true });
@@ -78,7 +99,7 @@ class Detailed extends Component {
   render() {
     const { props } = this;
     return (
-      <div className="Item">
+      <div className={`Item ${props.width <= 600 ? 'ItemHover' : ''}`}>
         <h4 className="ItemTitle">{props.expName}</h4>
         <div className="ProjectLinksContainer">
           {props.links.map(el => {
